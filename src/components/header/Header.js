db7839e6294eba067559e93f7703aad58f09d9e6
tangzekai <tangzekai@Handlecar.com>
2018-11-28 16:56:47 +0800

    home

---
+++ src/components/header/Header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { DatePicker  } from "antd";
+class Header extends Component {
+  render() {
+    return (
+      <div>
+        <DatePicker />
+      </div>
+    );
+  }
+}
+
+export default Header;
